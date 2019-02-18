187da899b8a48b55e36345d6c102bf8bba5caff0
Steve Kahn <steve@kahn.org.uk>
2019-02-18 15:39:45 +0000

    Added views folder to place erb files

--- app.rb
+++ app.rb
@@ -22,8 +22,6 @@ get '/shotgun' do
   "Shotgun works :)"
 end
 
-get '/cat' do 
-  "<div style= 'border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
-end 
+get '/cat' do
+  erb(:index)
+end
