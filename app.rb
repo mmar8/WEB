f1c0848141e32960a3ec688f0f06bf3ecee61f74
Maria Vanaale <mvanaale@gmail.com>
2019-02-18 16:29:57 +0000

    params

--- app.rb
+++ app.rb
@@ -22,7 +22,13 @@ get '/shotgun' do
   "Shotgun works :)"
 end
 
-get '/cat' do
+get '/random_cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named_cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
